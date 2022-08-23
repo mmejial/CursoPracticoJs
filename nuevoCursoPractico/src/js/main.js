8d5d5475656404db56c7aba3ba7d8745df6caeb0
Marco Antonio Mejia Lopez <tomatin77@gmail.com>
2022-08-23 18:49:20 -0500

    nose

--- nuevoCursoPractico/src/js/main.js
+++ nuevoCursoPractico/src/js/main.js
@@ -1,1 +1,15 @@
 const title = document.querySelector('.titulo');
+const inputs = document.querySelector('.input')
+const body = document.querySelector('body')
+const parrafo2 = document.querySelector('#parrafo2')
+console.log(body);
+console.log(inputs);
+/* inputs.value ="123" */
+
+const titulo2 = document.createElement('h2')
+
+titulo2.innerHTML =' titulo 2'
+console.log(titulo2);
+
+body.insertBefore(titulo2, inputs)
+
